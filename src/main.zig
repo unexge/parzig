acf2df308ce96c528f9043a5419cdc27d3c6cc20
Burak Varli <unexge@gmail.com>
2025-07-21 22:37:57 +0100

    Replace deprecated `GeneralPurposeAllocator` with `DebugAllocator`

--- src/main.zig
+++ src/main.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 const parzig = @import("parzig");
 
 pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    var gpa: std.heap.DebugAllocator(.{}) = .init;
     defer {
         _ = gpa.deinit();
     }
